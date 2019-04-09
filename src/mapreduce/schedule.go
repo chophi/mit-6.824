f876f7e97c5b24285b2d46404d093e7ebe974893
chophi <chophi@qq.com>
2019-04-10 00:34:31 +0800

    Lab1 Part III (not pass yet).
    Signed-off-by: chophi <chophi@qq.com>

--- src/mapreduce/schedule.go
+++ src/mapreduce/schedule.go
@@ -1,7 +1,7 @@
 package mapreduce
 
 import "fmt"
-
+import "sync"
 //
 // schedule() starts and waits for all tasks in the given phase (mapPhase
 // or reducePhase). the mapFiles argument holds the names of the files that
@@ -25,6 +25,47 @@ func schedule(jobName string, mapFiles []string, nReduce int, phase jobPhase, re
 
 	fmt.Printf("Schedule: %v %v tasks (%d I/Os)\n", ntasks, phase, n_other)
 
+	finished := make(chan bool)
+	var waitAllTasksComplete = sync.WaitGroup{}
+	waitAllTasksComplete.Add(ntasks)
+
+	go func() {
+		taskId := 0
+		taskRet := make(chan [2]int)
+		for {
+			select {
+			case tR := <- taskRet:
+				if tR[0] == 1 {
+					waitAllTasksComplete.Done()
+				}
+			case <- finished:
+				return
+			case rpc := <-registerChan:
+				taskArgs := DoTaskArgs {
+					JobName : jobName,
+					File : mapFiles[taskId],
+					Phase: phase,
+					TaskNumber : taskId,
+					NumOtherPhase : n_other,
+				}
+				go func() {
+					ret := call(rpc, "Worker.DoTask", &taskArgs, nil)
+					success := 0
+					if ret {
+						success = 1
+					}
+					taskState := [2]int {success, taskArgs.TaskNumber}
+					taskRet <- taskState
+				} ()
+				taskId++
+			}
+		}
+	} ()
+
+	go func() {
+		waitAllTasksComplete.Wait()
+		finished <- true
+	} ()
 	// All ntasks tasks have to be scheduled on workers. Once all tasks
 	// have completed successfully, schedule() should return.
 	//
